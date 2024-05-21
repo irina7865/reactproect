dd0bb020ebc502a553d11d2071afd209babe4e7c
unknown <you@example.com>
2024-05-21 20:05:05 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Componentnew from'./Component/Componentnew';
+
+function App() {
+  const new1 = 123
+  return (
+    <div className="App">Test{new1} <Componentnew text="text rfdfrfr" color="blue"/></div> 
+
+  );
+}
+export default App;
---
+++ src/Component/Componentnew.jsx
@@ -0,0 +1,11 @@
+import './Newcomp.css'
+
+function Componentnew ({text}){
+    return (
+      <div>  
+         <div className="newcomponet">{text}</div>
+      </div>
+)
+}
+
+export default Componentnew
